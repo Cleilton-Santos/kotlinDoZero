445d48e674be5f42e1dbca84ef5054ab6d96cfd5
Cleilton-Santos <cleilton14s@gmail.com>
2024-11-05 22:59:36 -0300

    acesso

---
+++ app/src/main/java/com/example/aprendendokotlindozero/NullSafety.kt
@@ -0,0 +1,15 @@
+package com.example.aprendendokotlindozero
+
+class Carro {
+    var cor = "Vermelho"
+
+    fun acelerar(){
+        // ***
+    }
+}
+fun main(){
+    var carro: Carro? = null
+    //carro = Carro()
+    // ***
+    println(carro?.cor)
+}
