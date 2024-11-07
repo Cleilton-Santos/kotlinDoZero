b2a84297a6e4a0df4d37c27028425b8bb7066285
Cleilton-Santos <cleilton14s@gmail.com>
2024-11-07 10:36:05 -0300

    modificaoes

--- app/src/main/java/com/example/aprendendokotlindozero/Operadores.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/Operadores.kt
@@ -1,2 +1,16 @@
 package com.example.aprendendokotlindozero
 
+fun main(){
+    //Operdaores basicos aritimetricos
+    /*val n1 = 10
+    val n2 = 20*/
+
+    //Operdores relacionais
+    /*val resultado = 2 > 5
+    val reultado = 8 == 5*/
+
+    //Operadores logicos
+    val resultado = true && true//true
+
+    println(resultado)
+}
