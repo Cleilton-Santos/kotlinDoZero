7dbd8bdb9526e0cf4e7ce6ab43eaa38bfcc0d498
Cleilton-Santos <cleilton14s@gmail.com>
2024-11-12 08:32:46 -0300

    alteracoes

--- app/src/main/java/com/example/aprendendokotlindozero/ifElseWhen.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/ifElseWhen.kt
@@ -1,11 +1,25 @@
 package com.example.aprendendokotlindozero
 
 fun main(){
-    val notaAluno = 8
-    val condicao = notaAluno >= 6
-    if (condicao){
-        println("Aluno aprovado")
+    /*val notaAluno = 8
+    val condicao = notaAluno >= 6*/
+
+    /*val opcao = 4
+
+    if (opcao == 1){
+        println("Cartao de credito")
+    }else if (opcao == 2){
+        println("Extrato Bancario")
+    }else if (opcao == 3){
+        println("Saldo")
     }else{
-        println("Aluno reprovado")
+        println("Nnehuma opcao selecionada")
+    }*/
+
+    val opcao = 4
+
+    if (opcao in 1..3){
+        println("Cartao de credito")
     }
+
 }
