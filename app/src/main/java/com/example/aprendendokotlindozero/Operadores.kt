340c49583454800a9b9cce8e156960cae769c6e4
Cleilton-Santos <cleilton14s@gmail.com>
2024-11-12 08:06:29 -0300

    aula if else

--- app/src/main/java/com/example/aprendendokotlindozero/Operadores.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/Operadores.kt
@@ -10,7 +10,14 @@ fun main(){
     val reultado = 8 == 5*/
 
     //Operadores logicos
-    val resultado = true && true//true
+    //val resultado = 2 > 1 && 4 == 3
+    //val resultado = true && true//true
 
+    //p1 50 anos comprou 100 reais
+    //p2 20 anos comprou 20 reais
+    val idadePessoa = 30
+    val comprasPessoa = 300
+
+    val resultado = idadePessoa >= 30 && comprasPessoa >= 400
     println(resultado)
 }
