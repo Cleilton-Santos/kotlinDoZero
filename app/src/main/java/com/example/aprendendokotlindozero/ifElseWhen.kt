c94127d97853897b3952ebc76f09faa3364194be
Cleilton-Santos <cleilton14s@gmail.com>
2024-11-12 08:15:48 -0300

    testes com exemplos

--- app/src/main/java/com/example/aprendendokotlindozero/ifElseWhen.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/ifElseWhen.kt
@@ -1,5 +1,11 @@
 package com.example.aprendendokotlindozero
 
 fun main(){
-
+    val notaAluno = 8
+    val condicao = notaAluno >= 6
+    if (condicao){
+        println("Aluno aprovado")
+    }else{
+        println("Aluno reprovado")
+    }
 }
