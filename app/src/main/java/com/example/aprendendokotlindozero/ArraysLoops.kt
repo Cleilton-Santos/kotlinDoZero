cfc73b042c4ca5fa60b41c5494a3d8106eb01669
Cleilton Santos <cleilton14s@gmail.com>
2024-11-16 07:53:48 -0300

    modificacao

--- app/src/main/java/com/example/aprendendokotlindozero/ArraysLoops.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/ArraysLoops.kt
@@ -2,9 +2,16 @@ package com.example.aprendendokotlindozero
 
 fun main(){
 
-    val nomes = arrayOf(
+    /*val nomes = arrayOf(
         "Jamilton", "Ana"
     )
-
+    nomes[0] = "mudou "
+    println(nomes[0])
+    */
+    var numero = 1
+    while (numero){
+        println("Executou: $numero")
+        numero++//incremento
+    }
 
 }
