f984220c8b55e05df003dd25cacb515761cde178
Cleilton Santos <cleilton14s@gmail.com>
2024-11-16 08:17:37 -0300

    modificacao

--- app/src/main/java/com/example/aprendendokotlindozero/ArraysLoops.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/ArraysLoops.kt
@@ -8,10 +8,24 @@ fun main(){
     nomes[0] = "mudou "
     println(nomes[0])
     */
-    var numero = 1
+    /*var numero = 1
     while (numero <= 5){
         println("Executou: $numero")
         numero++//incremento
+    }*/
+
+    /*for (numero in 1..4){
+        println("Executou: $numero")
+    }*/
+
+    val postagens = arrayOf(
+        "Viagem para a praia",
+        "Levei meu cahorro no veterinario",
+        "Trilha com os amigos"
+    )
+
+    for ((indice, postagem) in postagens.withIndex()){
+        println("Titulo: $postagem")
     }
 
 }
