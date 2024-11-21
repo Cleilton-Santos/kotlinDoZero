80018eea060d4ed66b93e36e34478b702cbcc2c8
Cleilton-Santos <cleilton14s@gmail.com>
2024-11-21 10:40:59 -0300

    testes de funcoes

--- app/src/main/java/com/example/aprendendokotlindozero/teste.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/teste.kt
@@ -1,16 +1,25 @@
 package com.example.aprendendokotlindozero
 
+/*
+fun fulano():Boolean = (true)
+fun sicrano():Boolean = (false)
+*/
+
 fun main(){
+
+
+    /*val retorno = fulano() == sicrano()
+    println(retorno)
+*/
     /*val motoristas = arrayOf(
         "Cleilton", "Tuila", "Ubiratan", "Acerola"
     )*/
-    val motoristas = 5
+    /*val motoristas = 5
     if (motoristas >= 5){
         println("Seja Bem Vindo")
     }else{
         println("Nenhuma escolha")
-    }
-
+    }*/
     /*val motoristas = 2
 
     for (betoneiros in motoristas){
