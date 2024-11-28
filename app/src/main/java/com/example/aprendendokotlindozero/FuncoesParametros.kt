1f582b8beb1212776575a6e96e5c2cd2cde61a14
Cleilton-Santos <cleilton14s@gmail.com>
2024-11-27 23:08:30 -0300

    modificacao

--- app/src/main/java/com/example/aprendendokotlindozero/FuncoesParametros.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/FuncoesParametros.kt
@@ -7,5 +7,6 @@ fun somar(numero1: Int, numero2: Int=0, numero3: Int = 0){//Definir a variavel c
 fun main(){
     //val numero:Int = 0, Do mesmo jeito que definimos uma variavel com o valor nulo
     //somar(numero1 = 10, numero2 = 50) Parametro nomeado
-    somar(10, 22, numero3 = 58)
+    //somar(10, 22, numero3 = 58)
+   somar(numero2 = 45, numero3 = 87, numero1 = 24)
 }
