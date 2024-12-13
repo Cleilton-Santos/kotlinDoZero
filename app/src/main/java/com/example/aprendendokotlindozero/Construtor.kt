8679d7b522db1f6d78bfd61dbfda1ebb003297f7
Cleilton Santos <cleilton14s@gmail.com>
2024-12-13 14:20:55 -0300

    aula construtor

--- app/src/main/java/com/example/aprendendokotlindozero/Construtor.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/Construtor.kt
@@ -1,19 +1,25 @@
 package com.example.aprendendokotlindozero
 
-class Usuario2 constructor(nome: String, sobrenome: String){//Construtor Primario
-
-    var nome: String = ""
+//Construtor Primario
+class Usuario2 (
+    var nome: String = "",
     var sobrenome: String = ""
+){
 
     init {//Inicializar
-        this.nome = nome
-        this.sobrenome = sobrenome
+        /*this.nome = nome
+        this.sobrenome = sobrenome*/
         println("Objeto inicializado")
         println("nome: $nome sobrenome: $sobrenome")
     }
+    //Constructor Secondary
+    constructor(nome: String): this(nome, ""){
+        println("Construtor Secundario")
+    }
+
 }
 
 fun main(){
-    val usuario = Usuario2("Cleilton", "Santos")
+    val usuario = Usuario2("Cleilton")
 
 }
